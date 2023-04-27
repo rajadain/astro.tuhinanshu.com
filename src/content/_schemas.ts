addc23a84967a5bb43349f965a4f47748a23ba83
Terence Tuhinanshu <terence@tuhinanshu.com>
2023-04-26 21:21:41 -0400

    Make post description optional
    This concept did not exist in Ghost, so none of those posts have it.
    Rather than add a dummy value, or a thoughtful value for every piece,
    we just make it optional for now. The UI is updated accordingly.

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -11,7 +11,7 @@ export const blogSchema = z
     draft: z.boolean().optional(),
     tags: z.array(z.string()).default(["others"]),
     ogImage: z.string().optional(),
-    description: z.string(),
+    description: z.string().optional(),
   })
   .strict();
 
