8ea4410f5c53508eabe6f10205cb5c31ff64aefa
Terence Tuhinanshu <terence@tuhinanshu.com>
2023-05-07 18:43:40 -0400

    feat(card): add an image preview to the cards
    There's now a preview of the open graph image, if specified on the post. Otherwise there's a spacer
    so the posts don't seem unaligned.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,25 +8,34 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, date_published, description } = frontmatter;
+  const { title, date_published, description, ogImage } = frontmatter;
   return (
-    <li className="my-6">
-      <a
-        href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-      >
-        {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
-            {title}
-          </h2>
-        ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
-            {title}
-          </h3>
-        )}
-      </a>
-      <Datetime datetime={date_published} />
-      <p>{description}</p>
+    <li className="my-6 flex items-center">
+      {ogImage ? (
+        <a className="mr-4 shrink-0 basis-20" href={href}>
+          <img className="h-20 w-20 rounded object-cover" src={ogImage} />
+        </a>
+      ) : (
+        <div className="mr-4 h-20 shrink-0 basis-20"></div>
+      )}
+      <div>
+        <a
+          href={href}
+          className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        >
+          {secHeading ? (
+            <h2 className="text-lg font-medium decoration-dashed hover:underline">
+              {title}
+            </h2>
+          ) : (
+            <h3 className="text-lg font-medium decoration-dashed hover:underline">
+              {title}
+            </h3>
+          )}
+        </a>
+        <Datetime datetime={date_published} />
+        <p>{description}</p>
+      </div>
     </li>
   );
 }
