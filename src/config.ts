7b4c9103e4bccdfdd0c72ff12c4a9f94bc9722e5
Terence Tuhinanshu <terence@tuhinanshu.com>
2023-06-26 23:32:37 -0400

    feat(logo): remove default logos

--- src/config.ts
+++ src/config.ts
@@ -10,13 +10,6 @@ export const SITE: Site = {
   postPerPage: 3,
 };
 
-export const LOGO_IMAGE = {
-  enable: false,
-  svg: true,
-  width: 216,
-  height: 46,
-};
-
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
