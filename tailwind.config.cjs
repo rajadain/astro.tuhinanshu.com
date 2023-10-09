e32e992dd98274695ebbdc5490653f6333d6a247
Terence Tuhinanshu <terence@tuhinanshu.com>
2023-10-08 20:48:10 -0400

    feat(tailwind): extend default Tailwind styles
    Otherwise we'd have to add back basic classes like bg-top and bg-center since those categories are
    overridden.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,60 +18,57 @@ module.exports = {
       sm: "640px",
     },
 
-    // Uncomment the following extend
-    // if existing Tailwind color palette will be used
-
-    // extend: {
-    textColor: {
-      skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
-        inverted: withOpacity("--color-fill"),
+    extend: {
+      textColor: {
+        skin: {
+          base: withOpacity("--color-text-base"),
+          accent: withOpacity("--color-accent"),
+          inverted: withOpacity("--color-fill"),
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: withOpacity("--color-fill"),
+          accent: withOpacity("--color-accent"),
+          inverted: withOpacity("--color-text-base"),
+          card: withOpacity("--color-card"),
+          "card-muted": withOpacity("--color-card-muted"),
+        },
+      },
+      backgroundImage: {
+        logo: "url('/assets/logo-sprite.png')",
+      },
+      backgroundPosition: {
+        "logo-position": "var(--logo-position)",
+        "logo-position-sm": "var(--logo-position-sm)",
+      },
+      backgroundSize: {
+        "logo-size": "64px 128px",
+        "logo-size-sm": "96px 192px",
+      },
+      outlineColor: {
+        skin: {
+          fill: withOpacity("--color-accent"),
+        },
+      },
+      borderColor: {
+        skin: {
+          line: withOpacity("--color-border"),
+          fill: withOpacity("--color-text-base"),
+          accent: withOpacity("--color-accent"),
+        },
+      },
+      fill: {
+        skin: {
+          base: withOpacity("--color-text-base"),
+          accent: withOpacity("--color-accent"),
+        },
+        transparent: "transparent",
+      },
+      fontFamily: {
+        mono: ["IBM Plex Mono", "monospace"],
       },
     },
-    backgroundColor: {
-      skin: {
-        fill: withOpacity("--color-fill"),
-        accent: withOpacity("--color-accent"),
-        inverted: withOpacity("--color-text-base"),
-        card: withOpacity("--color-card"),
-        "card-muted": withOpacity("--color-card-muted"),
-      },
-    },
-    backgroundImage: {
-      logo: "url('/assets/logo-sprite.png')",
-    },
-    backgroundPosition: {
-      "logo-position": "var(--logo-position)",
-      "logo-position-sm": "var(--logo-position-sm)",
-    },
-    backgroundSize: {
-      "logo-size": "64px 128px",
-      "logo-size-sm": "96px 192px",
-    },
-    outlineColor: {
-      skin: {
-        fill: withOpacity("--color-accent"),
-      },
-    },
-    borderColor: {
-      skin: {
-        line: withOpacity("--color-border"),
-        fill: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
-      },
-    },
-    fill: {
-      skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
-      },
-      transparent: "transparent",
-    },
-    fontFamily: {
-      mono: ["IBM Plex Mono", "monospace"],
-    },
-    // },
   },
   plugins: [require("@tailwindcss/typography")],
 };
