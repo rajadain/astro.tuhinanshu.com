a7cb2ea86d3ac2ae65bc650e0ee555badfbe47e5
Terence Tuhinanshu <terence@tuhinanshu.com>
2023-11-05 16:01:11 -0500

    feat(breadcrumb): always show full breadcrumb on every page
    Previously, Post Details did not have the full breadcrumb. Now they do. Additional changes are made
    to make them link correctly, and show tags and posts in the correct casing.

---
+++ src/utils/getBreadcrumbs.ts
@@ -0,0 +1,19 @@
+function isPositiveInteger(pathFragment: String): Boolean {
+  return /^\d+$/.test(pathFragment);
+}
+
+function getBreadcrumbs(currentUrlPath: String): String[] {
+  // Get url array from path
+  // eg: /tags/tailwindcss => ['tags', 'tailwindcss']
+  const breadcrumbList = currentUrlPath.split("/").slice(1);
+
+  // if breadcrumb is Home > Posts > 1 <etc>
+  // replace Posts with Posts (page number)
+  if (breadcrumbList[0] === "posts" && isPositiveInteger(breadcrumbList[1])) {
+    breadcrumbList.splice(0, 2, `Posts (page ${breadcrumbList[1] || 1})`);
+  }
+
+  return breadcrumbList;
+}
+
+export default getBreadcrumbs;
